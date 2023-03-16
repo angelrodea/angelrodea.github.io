43d1e95aff479548938c1bcaa34517813ef1a914
Angel RN <100503686+angelrodea@users.noreply.github.com>
2023-03-16 14:13:01 -0600

    Add files

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php include("template/header.php");?>
+    <main>
+        <div class="container">
+            <div>
+                <h3>Misión</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati hic natus eius error unde qui
+                    quasi repudiandae in sint culpa, enim placeat ad cupiditate atque tenetur odio a, assumenda officia!
+                </p>
+            </div>
+            <div>
+                <h3>Vsión</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati hic natus eius error unde qui
+                    quasi repudiandae in sint culpa, enim placeat ad cupiditate atque tenetur odio a, assumenda officia!
+                </p>
+            </div>
+            <div>
+                <h3>Misión</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati hic natus eius error unde qui
+                    quasi repudiandae in sint culpa, enim placeat ad cupiditate atque tenetur odio a, assumenda officia!
+                </p>
+            </div>
+        </div>
+        <div class="container-md">
+            <h3>Historia</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati hic natus eius error unde qui
+                quasi repudiandae in sint culpa, enim placeat ad cupiditate atque tenetur odio a, assumenda officia!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati hic natus eius error unde qui
+                quasi repudiandae in sint culpa, enim placeat ad cupiditate atque tenetur odio a, assumenda officia!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati hic natus eius error unde qui
+                quasi repudiandae in sint culpa, enim placeat ad cupiditate atque tenetur odio a, assumenda officia!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati hic natus eius error unde qui
+                quasi repudiandae in sint culpa, enim placeat ad cupiditate atque tenetur odio a, assumenda officia!
+                <br>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati hic natus eius error unde qui
+                quasi repudiandae in sint culpa, enim placeat ad cupiditate atque tenetur odio a, assumenda officia!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati hic natus eius error unde qui
+                quasi repudiandae in sint culpa, enim placeat ad cupiditate atque tenetur odio a, assumenda officia!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati hic natus eius error unde qui
+                quasi repudiandae in sint culpa, enim placeat ad cupiditate atque tenetur odio a, assumenda officia!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati hic natus eius error unde qui
+                quasi repudiandae in sint culpa, enim placeat ad cupiditate atque tenetur odio a, assumenda officia!
+            </p>
+        </div>
+    </main>
+<?php include("template/footer.php");?>
---
+++ template/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Escuela</title>
+</head>
+
+<body>
+    <header>
+        <div class="header-container">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark fixed-top" data-bs-theme="dark">
+                <div class="container-fluid px-3">
+                    <a class="navbar-brand" href="index.php"><img src="img/logo.png" style="height:30px;"></a>
+                    <span class="navbar-brand">Jean Piaget de oriente</span>
+                    <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarText">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item nav-mar">
+                                <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+                            </li>
+                            <!-- <li class="nav-item nav-mar">
+                                <a class="nav-link" href="#">Comunicados</a>  QUITAR Agregar a inicio
+                            </li>-->
+                            <li class="nav-item nav-mar">
+                                <a class="nav-link" href="#">Admisiones</a>
+                            </li>
+                            <!--<li class="nav-item dropdown nav-mar">
+                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                                    aria-expanded="false">Servicios</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Primaria</a></li>
+                                    <li><a class="dropdown-item" href="#">Secundaria</a></li>
+                                    <li><a class="dropdown-item" href="#">Preparatoria</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Separated link</a></li>
+                                </ul>
+                            </li> -->
+                            <li class="nav-item nav-mar">
+                                <a class="nav-link" href="#">Profesores</a>
+                            </li>
+                            <li class="nav-item nav-mar">
+                                <a class="nav-link" href="#">Comentarios</a>
+                            </li>
+                            <li class="nav-item nav-mar">
+                                <a class="nav-link" href="#">Contacto</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div class="jumbotron">
+                <h1>Jean Piaget de oriente</h1>
+                <p>Frase de algo!</p>
+            </div>
+        </div>
+    </header>
