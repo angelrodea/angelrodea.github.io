8262eb029818202c795490b87007b4f3cba14486
Angel RN <angelrodean@gmail.com>
2023-04-13 17:43:29 -0600

    Change PDO to MySQLi

--- db.php
+++ db.php
@@ -4,7 +4,17 @@
     $user="root";
     $pass="";
     try {
-        $conection= new PDO("mysql:host=$server;dbname=$db",$user,$pass);
+        $connection = new mysqli($server, $user, $pass, $db);    
+
+        if ($connection->connect_error) {
+            die("Conexión fallida: " . $connection->connect_error);
+        }
+        // Verificar si la conexión se realizó correctamente
+        #if ($connection) {
+        #    echo "Conexión exitosa a la base de datos";
+        #} else {
+            #echo "Error al conectar a la base de datos";
+        #}
     } catch (Exception $ex) {
         echo $ex->getMessage();
     }
