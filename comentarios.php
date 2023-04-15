a8c61f0dfe3f1abaa94f42ab03a9abdb686fe9f5
Angel RN <angelrodean@gmail.com>
2023-04-15 14:39:28 -0600

    Add delete button from admin

--- comentarios.php
+++ comentarios.php
@@ -3,7 +3,21 @@
         <div class="container">
             <h1 class="mb-4">Comentarios</h1>
             <div class="my-3">
-                <h4>Nombre: Natalia Jovanna Solar</h4>
+                <div class="mb-2 row">
+                    <h4 class="col-sm-11">Nombre: Natalia Jovanna Solar</h4>
+                    <div class="col-sm-1 ">
+                        <?php if (verifySession()): ?>
+                            <button type="button" class="btn btn-outline-danger pt-1 pb-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" 
+                                class="bi bi-x-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 
+                                    2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </button>
+                        <?php endif; ?>
+                    </div>
+                </div>
                 <div class="overflow-auto p-3 comentarios">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quas, laboriosam aliquam 
                     harum ducimus eligendi alias minus officiis nihil maxime ipsa saepe placeat dolorum deleniti incidunt. 
@@ -13,7 +27,21 @@
                 </div>
             </div>
             <div class="my-3">
-                <h4>Nombre: Javier Soldeño Guerrero</h4>
+                <div class="mb-2 row">
+                    <h4 class="col-sm-11">Nombre: Javier Soldeño Guerrero</h4>
+                    <div class="col-sm-1 ">
+                        <?php if (verifySession()): ?>
+                            <button type="button" class="btn btn-outline-danger pt-1 pb-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" 
+                                class="bi bi-x-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 
+                                    2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </button>
+                        <?php endif; ?>
+                    </div>
+                </div>
                 <div class="overflow-auto p-3 comentarios">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quas, laboriosam aliquam 
                     harum ducimus eligendi alias minus officiis nihil maxime ipsa saepe placeat dolorum deleniti incidunt. 
