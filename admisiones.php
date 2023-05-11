772326603624127d057d5384b3b5f09877c1e74a
Angel RN <angelrodean@gmail.com>
2023-05-11 11:25:48 -0600

    Update form

--- admisiones.php
+++ admisiones.php
@@ -1,7 +1,14 @@
 <?php include("template/header.php");?>
     <main>
         <div class="container">
-
+            <?php
+                if (isset($_GET['send'])) {
+                    echo "<div class='alert alert-success alert-dismissible fade show text-center' role='alert'>
+                            <strong>Se ha enviado correctamente sus datos, por favor revise su correo.</strong>
+                            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                        </div>";
+                }
+            ?>
             <!------------------- INFO ------------------------->
             <h1 class="display-5 ms-4">Costo</h1>
             <div class="jumbotron border rounded-3 bg-border p-3 mb-5">
@@ -20,7 +27,7 @@
                     illo pariatur quos voluptatum voluptatem reiciendis atque cupiditate.
                 </p>
             </div>
-            <center>
+            <center class="mb-4">
                 <h3 class="mb-3">¡Inscríbete aquí!</h3>
                 <button type="button" class="btn btn-dark btn-lg text-uppercase" data-bs-toggle="modal" 
                     data-bs-target="#filesModal">Inscripción</button>
@@ -29,131 +36,169 @@
         </div>
 
         <!-- Modal Files -->
-        <div class="modal fade" id="filesModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="filesModalLabel" aria-hidden="true">
+        <div class="modal fade" id="filesModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" 
+            aria-labelledby="filesModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Ingresar</h1>
+                        <h4 class="modal-title" id="exampleModalLabel">Datos personales</h4>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <form method="POST">
+                    <form action="db/insert_alumno.php" method="POST" enctype="multipart/form-data">
                         <div class="modal-body">
                             <div class="form-group row mb-3">
-                                <label class="col-lg-2 col-form-label" for="name">Nombre(s)</label>
+                                <label class="col-lg-2 col-form-label" for="nombre">Nombre(s)</label>
                                 <div class="col-lg-10">
-                                    <input type="text" class="form-control" id="name" name="name" placeholder="ej. Maria" required>
+                                    <input type="text" class="form-control" id="nombre" name="nombre" placeholder="ej. Maria" required>
                                 </div>
                             </div>
                             <div class="form-group row mb-3">
-                                <label class="col-lg-2 col-form-label" for="name">Ap. Paterno</label>
+                                <label class="col-lg-2 col-form-label" for="apPaterno">Ap. Paterno</label>
                                 <div class="col-lg-4">
-                                    <input type="text" class="form-control" id="name" name="name" placeholder="Maria" required>
+                                    <input type="text" class="form-control" id="apPaterno" name="apPaterno" placeholder="Maria" required>
                                 </div>
-                                <label class="col-lg-2 col-form-label" for="name">Ap. Materno</label>
+                                <label class="col-lg-2 col-form-label" for="apMaterno">Ap. Materno</label>
                                 <div class="col-lg-4">
-                                    <input type="text" class="form-control" id="name" name="name" placeholder="Maria" required>
+                                    <input type="text" class="form-control" id="apMaterno" name="apMaterno" placeholder="Maria" required>
                                 </div>
                             </div>
                             <div class="form-group row mb-3">
-                                <label class="col-lg-2 col-form-label" for="name">Edad</label>
-                                <div class="col-lg-10">
-                                    <input type="number" class="form-control" id="name" name="name" placeholder="ej. 8" required>
+                                <label class="col-lg-2 col-form-label" for="edad">Edad</label>
+                                <div class="col-lg-4">
+                                    <input type="number" class="form-control" id="edad" name="edad" placeholder="ej. 8" required>
+                                </div>
+                                <label class="col-lg-2 col-form-label" for="nacimiento">Fecha de nacimeinto</label>
+                                <div class="col-lg-4">
+                                    <input type="date" class="form-control" id="nacimiento" name="nacimiento" required>
                                 </div>
                             </div>
                             <div class="form-group row mb-3">
-                                <label class="col-lg-3 col-form-label" for="name">Correo electrónico</label>
+                                <label class="col-lg-2 col-form-label" for="curp">CURP</label>
+                                <div class="col-lg-4">
+                                    <input type="text" class="form-control" id="curp" name="curp" placeholder="ej. DWAD1412AWD12" required>
+                                </div>
+                                <label class="col-lg-2 col-form-label" for="curp">Género</label>
+                                <div class="col-lg-4">
+                                    <select class="form-select" name="genero" aria-label="Default select example">
+                                        <option selected>Seleccionar</option>
+                                        <option value="Masculino">Masculino</option>
+                                        <option value="Femenino">Femenino</option>
+                                        <option value="Otro">Otro</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group row mb-3">
+                                <label class="col-lg-3 col-form-label" for="correo">Correo electrónico</label>
                                 <div class="col-lg-9">
-                                    <input type="email" class="form-control" id="name" name="name" placeholder="ej. maria@gmail.com" 
+                                    <input type="email" class="form-control" id="correo" name="correo" placeholder="ej. maria@gmail.com" 
                                         required>
                                 </div>
                             </div>
                             <div class="form-group row mb-3">
-                                <label class="col-lg-2 col-form-label" for="name">Teléfono</label>
+                                <label class="col-lg-2 col-form-label" for="tel">Teléfono</label>
                                 <div class="col-lg-10">
-                                    <input type="tel" class="form-control" id="name" name="name" placeholder="ej. 55325426171" 
+                                    <input type="tel" class="form-control" id="tel" name="tel" placeholder="ej. 55325426171" 
                                         required>
                                 </div>
                             </div>
                             <!-- Campo para seleccionar el grado actual -->
                             <div class="form-group row mb-3">
-                                <label class="col-lg-2 col-form-label" for="name">Grado actual</label>
+                                <label class="col-lg-2 col-form-label" for="grado">Grado actual</label>
                                 <div class="col-lg-10">
-                                    <select class="form-control" name="grado_actual" id="grado_actual" required>
+                                    <select class="form-control" name="grado" id="grado" required>
                                         <option value="">Seleccione un grado</option>
                                         <option value="1">1°</option>
                                         <option value="2">2°</option>
                                         <option value="3">3°</option>
                                         <option value="4">4°</option>
                                         <option value="5">5°</option>
-                                        <option value="6">Ninguno</option>
+                                        <option value="Ninguno">Ninguno</option>
                                     </select>
                                 </div>
                             </div>
                             <div class="form-group row mb-3">
-                                <label class="col-lg-4 col-form-label" for="name">Nombe del padre o tutor</label>
+                                <label class="col-lg-4 col-form-label" for="tutor">Nombe del padre o tutor</label>
                                 <div class="col-lg-8">
-                                    <input type="text" class="form-control" id="name" name="name" placeholder="ej. Juan Quiroz Narvarte" 
+                                    <input type="text" class="form-control" id="tutor" name="tutor" placeholder="ej. Juan Quiroz Narvarte" 
+                                        required>
+                                </div>
+                            </div>
+                            <!---------------- DIRECCION ------------------>
+                            <h4 class="mt-5">Dirección</h4>
+                            <hr class="mb-3">
+                            <div class="form-group row mb-3">
+                                <label class="col-lg-1 col-form-label" for="calle">Calle</label>
+                                <div class="col-lg-3">
+                                    <input type="text" class="form-control" id="calle" name="calle" placeholder="ej. Calle 1" 
+                                        required>
+                                </div>
+                                <label class="col-lg-1 col-form-label" for="num">Número</label>
+                                <div class="col-lg-3">
+                                    <input type="text" class="form-control" id="num" name="num" placeholder="ej. 552172617" 
+                                        required>
+                                </div>
+                                <label class="col-lg-1 col-form-label" for="col">Colonia</label>
+                                <div class="col-lg-3">
+                                    <input type="text" class="form-control" id="col" name="col" placeholder="ej. Tepalcates" 
                                         required>
                                 </div>
                             </div>
                             <div class="form-group row mb-3">
-                                <label class="col-lg-2 col-form-label" for="name">Dirección</label>
-                                <div class="col-lg-10">
-                                    <input type="text" class="form-control" id="name" name="name" placeholder="ej. Calle 1" 
+                                <label class="col-lg-1 col-form-label" for="cp">C.P.</label>
+                                <div class="col-lg-2">
+                                    <input type="text" class="form-control" id="cp" name="cp" placeholder="ej. 11899" 
+                                        required>
+                                </div>
+                                <label class="col-lg-2 col-form-label" for="mun">Municipio</label>
+                                <div class="col-lg-3">
+                                    <input type="text" class="form-control" id="mun" name="mun" placeholder="ej. Iztapalapa" 
+                                        required>
+                                </div>
+                                <label class="col-lg-1 col-form-label" for="entidad">Entidad</label>
+                                <div class="col-lg-3">
+                                    <input type="text" class="form-control" id="entidad" name="entidad" placeholder="ej. CDMX" 
                                         required>
                                 </div>
                             </div>
+                            <!---------------- DOCUMEMTOS ------------------>
+                            <h4 class="mt-5">Documentos</h4>
+                            <hr class="mb-3">
                             <div class="form-group row mb-2">
-                                <label class="col-lg-3 col-form-label" for="name">Acta de nacimiento</label>
+                                <label class="col-lg-3 col-form-label" for="doc_acta">Acta de nacimiento</label>
                                 <div class="col-lg-9 mb-2">
-                                    <input type="file" class="form-control" name="archivo_acta">
+                                    <input type="file" class="form-control" name="doc_acta" required>
                                 </div>
-                                <label class="col-lg-3 col-form-label" for="name">CURP</label>
+                                <label class="col-lg-3 col-form-label" for="doc_curp">CURP</label>
                                 <div class="col-lg-9 mb-2">
-                                    <input type="file" class="form-control" name="archivo_curp">
+                                    <input type="file" class="form-control" name="doc_curp" required>
                                 </div>
-                                <label class="col-lg-3 col-form-label" for="name">Certificado médico</label>
+                                <label class="col-lg-3 col-form-label" for="doc_medico">Certificado médico</label>
                                 <div class="col-lg-9 mb-2">
-                                    <input type="file" class="form-control" name="archivo_medico">
+                                    <input type="file" class="form-control" name="doc_medico" required>
                                 </div>
-                                <label class="col-lg-3 col-form-label" for="name">Formato inscripción</label>
+                                <label class="col-lg-3 col-form-label" for="doc_inscripcion">Formato inscripción</label>
                                 <div class="col-lg-9 mb-2">
-                                    <input type="file" class="form-control" name="archivo_inscripcion">
+                                    <input type="file" class="form-control" name="doc_inscripcion" required>
                                 </div>
-                                <label class="col-lg-3 col-form-label" for="name">INE del tutor</label>
+                                <label class="col-lg-3 col-form-label" for="doc_ine">INE del tutor</label>
                                 <div class="col-lg-9 mb-2">
-                                    <input type="file" class="form-control" name="archivo_ine">
+                                    <input type="file" class="form-control" name="doc_ine" required>
                                 </div>
-                                <label class="col-lg-3 col-form-label" for="name">Comprobante de domicilio</label>
+                                <label class="col-lg-3 col-form-label" for="doc_domicilio">Comprobante de domicilio</label>
                                 <div class="col-lg-9 mb-2">
-                                    <input type="file" class="form-control" name="archivo_comprobanteDireccion">
+                                    <input type="file" class="form-control" name="doc_domicilio" required>
                                 </div>
                             </div>
                             <!-- Campo para subir la boleta -->
                             <div class="mb-2" id="campo_boleta" style="display: none;">
                                 <label class="form-label">Subir boleta del grado actual</label>
-                                <input type="file" class="form-control" name="archivo_boleta">
+                                <input type="file" class="form-control" name="doc_boleta">
                             </div>
                             <!-- Campo para subir la constancia de kinder -->
                             <div class="mb-2" id="campo_kinder" style="display: none;">
                                 <label class="form-label">Subir Constancia kinder </label>
-                                <input type="file" class="form-control" name="archivo_kinder">
+                                <input type="file" class="form-control" name="doc_kinder">
                             </div>
- 
-                            <!--<div class="mb-3">
-                                <label class="form-label">Subir Acta de nacimiento</label>
-                                <input type="file" class="form-control" name="archivo_acta">
-                                <label class="form-label">Subir CURP</label>
-                                <input type="file" class="form-control" name="archivo_curp">
-                                <label class="form-label">Subir archivo medico</label>
-                                <input type="file" class="form-control" name="archivo_medico">
-                                <label class="form-label">Subir inscripción </label>
-                                <input type="file" class="form-control" name="archivo_inscripcion">
-                                <label class="form-label">Subir INE del tutor </label>
-                                <input type="file" class="form-control" name="archivo_ine">
-                                <label class="form-label">Subir Comprobante de dirección </label>
-                                <input type="file" class="form-control" name="archivo_comprobanteDireccion">
-                            </div>-->
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
