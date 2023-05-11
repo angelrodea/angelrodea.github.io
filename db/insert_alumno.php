a9395056f2c38d4bf1a99e35d3d5390f78a84c89
Angel RN <angelrodean@gmail.com>
2023-05-11 15:26:19 -0600

    Add alumno from admisiones.php

---
+++ db/insert_alumno.php
@@ -0,0 +1,188 @@
+<?php
+    /* 
+        ESTRUCTURA: primer letra de nombre - primera palabra del apellido paterno - primer letra del apellido materno
+    */
+    function generarCorreoYPass($nombre, $apPaterno, $apMaterno, $idAlumno) {
+        include("db.php");
+        $nombreInicial = strtolower(substr($nombre, 0, 1));
+    
+        $apPaternoArray = explode(' ', $apPaterno);
+        $apPaternoPrimeraPalabra = strtolower($apPaternoArray[0]);
+    
+        $apMaternoInicial = strtolower(substr($apMaterno, 0, 1));
+    
+        // Generar una contraseña aleatoria de 12 caracteres
+        $password = generarPassword(12);
+
+        $correo = $nombreInicial . $apPaternoPrimeraPalabra . $apMaternoInicial . $idAlumno . "@gmail.com";
+
+        // INSERT a USUARIO
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO usuario (correo, password) VALUES (?, ?)";
+        $stmt = $connection->prepare($sql);
+        $stmt->bind_param("ss", $correo, $hashed_password);
+
+        if ($stmt->execute()) {
+            $idUsuario = mysqli_insert_id($connection);
+            // Actualizar la tabla "alumno"
+            $sqlUpdate = "UPDATE alumno SET id_usuario = ? WHERE id_alumno = ?";
+            $stmtUpdate = $connection->prepare($sqlUpdate);
+            $stmtUpdate->bind_param("ii", $idUsuario, $idAlumno);
+            
+            if ($stmtUpdate->execute()) {
+                return array("correo" => $correo, "password" => $password);
+            } else {
+                echo "Error al actualizar la tabla alumno: " . $stmtUpdate->error;
+            }
+            $stmtUpdate->close();
+        } else {
+            echo "Error al insertar en la tabla usuario: " . $stmt->error;
+        }
+        $stmt->close();
+        $connection->close();
+    
+    }
+    
+    function generarPassword($longitud) {
+        $caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789$%&*#";
+        $password = "";
+
+        for ($i = 0; $i < $longitud; $i++) {
+            $randomIndex = rand(0, strlen($caracteres) - 1);
+            $password .= $caracteres[$randomIndex];
+        }
+
+        return $password;
+    }
+
+    function enviarCorreo($email, $password) {
+        $msg="";
+        $para = "jeanpigetoriente@gmail.com"; // CAMBIAR por $email
+        $asunto = "Inscripción Jean Piget Oriente";
+        $mensaje = "<html>
+                        <head>
+                            <title>Continue con su proceso</title>
+                        </head>
+                        <body>
+                            <p>Porfvor guarde la siguiente información y no la comparta con nadie</p>
+                            <p>Su cuenta es: <b>$email</b></p>
+                            <p>Su contraseña es: <b>$password</b></p>
+                            <p>Gracias</p>
+                        </body>
+                    </html>";
+
+        $cabeceras = "From: jeanpigetoriente@gmail.com" . "\r\n" .
+            "Reply-To: jeanpigetoriente@gmail.com" . "\r\n" .
+            "Content-type: text/html; charset=utf-8" . "\r\n" .
+            "X-Mailer: PHP/" . phpversion();
+
+        // Envío del correo electrónico
+        if(mail($para, $asunto, $mensaje, $cabeceras)) {
+            header("Location: http://localhost/app/admisiones.php?send");
+            exit;
+        } else {
+            $msg .= "<div class='container'><div class='alert alert-danger mt-4' role='alert'>
+                        Hubo un error al enviar el correo
+                    </div></div>";
+            echo $msg;
+        }
+    }
+
+
+    if (isset($_POST['btnRegistro'])) {
+        include("db.php");
+        date_default_timezone_set('America/Mexico_City');
+        $date = date('Y-m-d');
+
+        $i = 0;
+        $null = NULL;
+        $status = 'solicitante';
+        $url = "http://localhost/app/uploads/";
+        $uploadDirectory = "../uploads/";
+        $archivos = $_FILES;
+        $validacion = 'pendiente';
+        $documento = [
+            'Acta de nacimiento',
+            'CURP',
+            'Certificado médico',
+            'Formato inscripción',
+            'INE del tutor',
+            'Comprobante de domicilio',
+            'Documento de boleta/kinder'
+        ];
+
+        // Insertar el registro en la tabla "grupo"
+        $grado = mysqli_real_escape_string($connection, $_POST['grado']);
+        $sqlGrupo = "INSERT INTO grupo (id_profesor, grupo) VALUES (?, ?)";
+        $stmtGrupo = $connection->prepare($sqlGrupo);
+        $stmtGrupo->bind_param("is", $null, $grado);
+
+        if ($stmtGrupo->execute()) {
+            // Obtener el ID del grupo recién insertado 
+            $idGrupo = $stmtGrupo->insert_id;
+
+            $nombre = mysqli_real_escape_string($connection, $_POST['nombre']);
+            $apPaterno = mysqli_real_escape_string($connection, $_POST['apPaterno']);
+            $apMaterno = mysqli_real_escape_string($connection, $_POST['apMaterno']);
+            $edad = mysqli_real_escape_string($connection, $_POST['edad']);
+            $fecha_nacimiento = mysqli_real_escape_string($connection, $_POST['nacimiento']);
+            $curp = mysqli_real_escape_string($connection, $_POST['curp']);
+            $genero = mysqli_real_escape_string($connection, $_POST['genero']);
+            $correo = mysqli_real_escape_string($connection, $_POST['correo']);
+            $celular = mysqli_real_escape_string($connection, $_POST['tel']);
+            $tutor = mysqli_real_escape_string($connection, $_POST['tutor']);
+
+            $dir_calle = mysqli_real_escape_string($connection, $_POST['calle']);
+            $dir_numero = mysqli_real_escape_string($connection, $_POST['num']);
+            $dir_colonia = mysqli_real_escape_string($connection, $_POST['col']);
+            $dir_cp = mysqli_real_escape_string($connection, $_POST['cp']);
+            $dir_municipio = mysqli_real_escape_string($connection, $_POST['mun']);
+            $dir_entidad = mysqli_real_escape_string($connection, $_POST['entidad']);
+
+
+            $sqlAlumno = "INSERT INTO alumno (id_usuario, id_grupo, status, nombre, ap_paterno, ap_materno, edad, curp, 
+                        fecha_nacimiento, genero, celular, correo, tutor, dir_calle, dir_numero, dir_colonia, dir_municipio, 
+                        dir_cp, dir_entidad) 
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $stmtAlumno = $connection->prepare(trim($sqlAlumno));
+            $stmtAlumno->bind_param("iisssssssssssssssss", $idUsuario, $idGrupo, $status, $nombre, $apPaterno, $apMaterno, $edad, 
+                                            $curp, $fecha_nacimiento, $genero, $celular, $correo, $tutor, $dir_calle, $dir_numero, 
+                                            $dir_colonia, $dir_municipio, $dir_cp, $dir_entidad);
+
+            if ($stmtAlumno->execute()) {
+                // Obtener el ID del alumno recién insertado
+                $idAlumno = $stmtAlumno->insert_id;
+
+                // Recorrer los archivos recibidos
+                foreach ($archivos as $nombreArchivo => $archivo) {
+                    $nombreArchivo = $idAlumno . "-" . basename($archivo['name']);
+                    $rutaArchivo = $uploadDirectory . $nombreArchivo;
+                    $rutaBase = $url . $nombreArchivo;
+
+                    // Mover el archivo a la carpeta de destino
+                    if (move_uploaded_file($archivo['tmp_name'], $rutaArchivo)) {
+                        // Insertar información del archivo en la base de datos
+                        $sqlArchivo = "INSERT INTO documento (id_alumno, documento, archivo, validacion, fecha) VALUES (?, ?, ?, ?, ?)";
+                        $stmtArchivo = $connection->prepare($sqlArchivo);
+                        $docActual = $documento[$i];
+
+                        $stmtArchivo->bind_param("issss", $idAlumno, $docActual, $rutaBase, $validacion, $date);
+                        $stmtArchivo->execute();
+                        $stmtArchivo->close();
+                        $i += 1;
+                    }
+                }
+                $resultado = generarCorreoYPass($nombre, $apPaterno, $apMaterno, $idAlumno);
+                $email = $resultado["correo"];
+                $password = $resultado["password"];
+                enviarCorreo($email, $password);
+            } else {
+                echo 'ERROR :(';
+            }
+
+            $stmtAlumno->close();
+            $stmtGrupo->close();
+            $connection->close();
+        }
+    }
+?>
