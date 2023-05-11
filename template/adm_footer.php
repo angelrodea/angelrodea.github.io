7257b1174fb894025235b66b889d3512cc1963d8
Angel RN <angelrodean@gmail.com>
2023-05-11 11:44:28 -0600

    Update path to JS

--- template/adm_footer.php
+++ template/adm_footer.php
@@ -11,6 +11,6 @@
     crossorigin="anonymous">
 </script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="js/main.js"></script>
+<script src="../js/main.js"></script>
 
 </html>
