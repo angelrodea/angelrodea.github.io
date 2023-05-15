17978f18184dfe08461a6ff08b9b81c634e5b3e9
Angel RN <angelrodean@gmail.com>
2023-05-14 19:51:03 -0600

    Delete student

---
+++ db/delete_alumno.php
@@ -0,0 +1,33 @@
+<?php
+    if (isset($_GET['id'])) {
+        include("db.php");
+        $id = $_GET['id'];
+
+        // Eliminar los registros relacionados en la tabla "documento"
+        $sqlDocumento = "DELETE FROM documento WHERE id_alumno = ?";
+        $stmtDocumento = $connection->prepare($sqlDocumento);
+        $stmtDocumento->bind_param("i", $id);
+
+        if ($stmtDocumento->execute()) {
+            // Eliminar el alumno de la tabla "alumno"
+            $sqlAlumno = "DELETE FROM alumno WHERE id_alumno = ?";
+            $stmtAlumno = $connection->prepare($sqlAlumno);
+            $stmtAlumno->bind_param("i", $id);
+
+            if ($stmtAlumno->execute()) {
+                header("Location: http://localhost/app/admin/solicitantes.php");
+                exit;
+            } else {
+                echo "Error al eliminar el alumno: " . $stmtAlumno->error;
+            }
+        } else {
+            echo "Error al eliminar los registros relacionados en la tabla documento: " . $stmtDocumento->error;
+        }
+
+        $stmtDocumento->close();
+        $stmtAlumno->close();
+        $connection->close();
+    } else {
+        echo "No se proporcionó el ID del alumno.";
+    }
+?>
