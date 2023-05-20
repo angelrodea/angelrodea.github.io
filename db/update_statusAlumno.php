8790806ef97f4b7bb87cf5a931fd2a80efd5d864
Angel RN <angelrodean@gmail.com>
2023-05-20 16:11:36 -0600

    Add 2nd update status

--- db/update_statusAlumno.php
+++ db/update_statusAlumno.php
@@ -1,6 +1,6 @@
 <?php
+    include("db.php");
     if (isset($_GET['id'])) {
-        include("db.php");
         $id = $_GET['id'];
         $sql = "UPDATE alumno SET status = 'pendiente' WHERE id_alumno = ?";
         $stmt = $connection->prepare($sql);
@@ -13,7 +13,21 @@
             echo "Error al actualizar el status del alumno: " . $stmt->error;
         }
 
-        // Cerrar la declaración preparada y la conexión
+        $stmt->close();
+        $connection->close();
+    } elseif (isset($_GET['id2'])) {
+        $id = $_GET['id2'];
+        $sql = "UPDATE alumno SET status = 'grupos' WHERE id_alumno = ?";
+        $stmt = $connection->prepare($sql);
+        $stmt->bind_param("i", $id);
+
+        if ($stmt->execute()) {
+            header("Location: http://localhost/app/admin/pendientes.php");
+            exit;
+        } else {
+            echo "Error al actualizar el status del alumno: " . $stmt->error;
+        }
+
         $stmt->close();
         $connection->close();
     } else {
