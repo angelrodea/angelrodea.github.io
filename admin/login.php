b8f8aa8b1b15c071a576d8cad51b16296af63df9
Angel RN <angelrodean@gmail.com>
2023-05-20 15:47:32 -0600

    Add session variable

--- admin/login.php
+++ admin/login.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    error_reporting(0);
     include("../db/db.php");
+    $mensaje="";
 
     if (isset($_POST['access'])){ 
         $ruser = mysqli_real_escape_string($connection, $_POST['user']);
@@ -21,8 +21,7 @@
         $connection->close();
         if (isset($ruser) && isset($rpass)) {
             if ($ruser == $userok && password_verify($rpass, $passok)){
-                $_SESSION['login'] = TRUE;
-                $_SESSION['user'] = $ruser;
+                $_SESSION['admin'] = $ruser;
                 header("Location: http://localhost/app/admin/solicitantes.php");
             } else {
                 $mensaje.="<div class='container'><div class='alert alert-danger mt-4' role='alert'>
@@ -30,13 +29,6 @@
                             </div></div>";
             }
         }
-    }
-    # LOGOUT
-    if (isset($_POST['logout'])) { // Comprobar si se ha enviado el formulario de cierre de sesión
-        session_unset(); // Eliminar todas las variables de sesión
-        session_destroy(); // Destruir la sesión
-        header("Location: http://localhost/app/index.php"); // Redirigir a la página de inicio de sesión
-        exit;
     }
 ?>
 
@@ -64,8 +56,8 @@
                                 <div class="card-body p-5 text-center">
                                     <div class="mb-md-5 mt-md-4 pb-5">
                                         <div class="login-box">
-                                            <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-                                            <p class="text-white-50 mb-5">Please enter your user and password!</p>
+                                            <h2 class="fw-bold mb-2 text-uppercase">Iniciar sesión</h2>
+                                            <p class="text-white-50 mb-5">Por favor ingrese su correo y contraseña!</p>
                                             <form method="POST">
                                                 <div>
                                                     <input type="text" name="user" class="form-control rounded-left mb-4" 
@@ -96,48 +88,6 @@
                 </div>
         </section>
     </main>
-    <!--
-    <section class="vh-100 gradient-custom">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
-                        <div class="card-body p-5 text-center">
-                            <div class="mb-md-5 mt-md-4 pb-5">
-                                <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-                                <p class="text-white-50 mb-5">Please enter your login and password!</p>
-                                <div class="form-outline form-white mb-4">
-                                    <input type="email" id="typeEmailX" class="form-control form-control-lg" require />
-                                    <label class="form-label" for="typeEmailX">Email</label>
-                                </div>
-
-                                <div class="form-outline form-white mb-4">
-                                    <input type="password" id="typePasswordX" class="form-control form-control-lg" />
-                                    <label class="form-label" for="typePasswordX">Password</label>
-                                </div>
-
-                                <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
-
-                                <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-
-                                <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                                    <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
-                                    <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
-                                    <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
-                                </div>
-                            </div>
-                            <div>
-                                <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
-                                </p>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    --> 
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
